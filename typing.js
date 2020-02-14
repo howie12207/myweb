0a9dd88a0ef9228fc7c88803964d5d3100f17d6d
Howie <howie12207@gmail.com>
2020-02-14 12:12:29 +0800

    update

---
+++ typing.js
@@ -0,0 +1,53 @@
+/* Typing.js - animates typing effect.
+ * Author: Lihui
+ * Websitte: withlihui.com
+ * Github: github.com/lihuii/typing.js
+ * MIT licesned.
+ */
+
+var b = 0;
+var c = 0;
+var isTyping = true;
+var isLoop = true;
+var isPlaceholder = false;
+var i = 0;
+var typingSpeed = 200;
+var deleteSpeed = 40;
+var waitTime = 1000;
+var strings = new Array("Hello I'm Howie !!", "Welcome to my website.");
+setTimeout("typing()", waitTime);
+
+function typing() {
+  if (isPlaceholder == true) {
+    document.getElementById("words").placeholder = strings[i].substring(c, b);
+  } else {
+    document.getElementById("words").innerHTML = strings[i].substring(c, b);
+  }
+
+  if (b == strings[i].length) {
+    setTimeout("b=0, c=strings[i].length, isTyping=true", waitTime);
+  } else {
+    if (c != 0) {
+      c--;
+      if (i == strings.length - 1 && isLoop == false) {
+        return;
+      }
+
+      if (c == 0) {
+        i++;
+        if (i == strings.length) {
+          i = 0;
+        }
+      }
+    } else {
+      var isTyping = false;
+      b++;
+    }
+  }
+
+  if (isTyping == false) {
+    setTimeout("typing()", typingSpeed);
+  } else {
+    setTimeout("typing()", deleteSpeed);
+  }
+}
